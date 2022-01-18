1fcea08c0ca410b20eacecf48cfd3aa1e1c0c700
roywnsdud <roywnsdud@naver.com>
2022-01-18 15:25:36 +0900

    FEAT: 메인 및 대기실 화면 스켈레톤

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,34 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <img src="../assets/logo.png" alt="logo">
+    <a href="#">로그인 하기</a>
+    <br>
+    <router-link :to="{ name: 'Room' }">대기실 가기</router-link>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  // import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
+    // components: {
+    //   HelloWorld,
+    // },
   }
 </script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;  
+}
+.home img {
+  width: 300px;
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+</style>
---
+++ frontend/src/views/Room.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <img src="../assets/logo.png" alt="logo" width="200">
+    <v-container class="player-row">
+      <v-row>
+
+      <div v-for="player in players" :key="player.id">
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>          
+            <v-list-item-title class="text-h5 mb-1">
+              {{player.id}}
+            </v-list-item-title>          
+          </v-list-item-content>
+        </v-list-item>
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      </div>
+      
+
+    
+      </v-row>
+    </v-container>
+    <div>
+
+
+    <v-container class="player-col">
+      <v-col>
+      <div v-for="player in players" :key="player.id">
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>          
+            <v-list-item-title class="text-h5 mb-1">
+              {{player.id}}
+            </v-list-item-title>          
+          </v-list-item-content>
+        </v-list-item>
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      </div>
+      </v-col>      
+    </v-container>
+    <div>
+      <img src="../assets/logo.png" alt="logo" width="800">
+      </div>
+          </div>
+    
+
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: "Room",
+  
+  data: function () {
+    return { 
+      players : [
+        {id : "player1"},
+        {id : "player2"},
+        {id : "player3"},
+        {id : "player4"},        
+      ]
+  }
+  }
+}
+</script>
+
+
+<style>
+.player-col {
+  display:flex;
+  margin: 0;
+}
+</style>
