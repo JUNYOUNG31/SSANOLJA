7141861f3a7a3ce3c78ca4ac31989a3781f64c42
roywnsdud <roywnsdud@naver.com>
2022-01-18 11:46:44 +0900

    FIX: vuetify 실행오류 해결

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -38,54 +38,18 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
-        <v-carousel v-model="model">
-    <v-carousel-item
-      v-for="(color, i) in colors"
-      :key="color"
-    >
-      <v-sheet
-        :color="color"
-        height="100%"
-        tile
-      >
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="text-h2">
-            Slide {{ i + 1 }}
-          </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
+      <router-view/>
     </v-main>
   </v-app>
-
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
-    model: 0,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
-      ],
   }),
 };
 </script>
