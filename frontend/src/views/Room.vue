33b5792ee8560217736de8d4fb8d2d4e295e10a9
roywnsdud <roywnsdud@naver.com>
2022-01-27 14:04:16 +0900

    FEAT: 게임선택 후 시작시 해당게임 정보를 전달하게 설정

--- frontend/src/views/Room.vue
+++ frontend/src/views/Room.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="room">
     <v-container><!--세로배열-->
-      <v-row class="row-cols-5 div1" style="border: hidden"> <!--div1 로고 & 화면4개--><!--가로배열-->
-        <v-col class="div1-1"><img src="../assets/logo.png" alt="logo"></v-col> <!--div1-1 로고-->
+      <v-row class="div1" style="border: hidden"> <!--div1 로고 & 화면4개--><!--가로배열-->
+        <v-col class="div1-1">
+          <img class="img1" src="../assets/logo.png" alt="logo" @click="leaveSession"> 
+          </v-col> <!--div1-1 로고-->
         <v-col class="playercamera">
           <user-video :stream-manager="publisher" @click.native="updateMainVideoStreamManager(publisher)"/>
         </v-col>
@@ -20,21 +22,21 @@
           <v-row class="div3"><!--div3 게임선택 및 레디 --><!--가로배열-->
             <v-row class="div3-1 row-cols-3"> <!--div3-1 게임선택--><!--가로배열-->
               <v-col>
-                <v-btn>스파이폴</v-btn>
+                <v-btn v-bind:class="{'grey': gameSelected == 'spyfall'}" @click="gameSelect('spyfall')">스파이폴 {{gameSelected}}</v-btn>
               </v-col>
               <v-col>
-                <v-btn>가예누가</v-btn>
+                <v-btn v-bind:class="{'grey': gameSelected == 'fakeartist'}" @click="gameSelect('fakeartist')">가예누가</v-btn>
               </v-col>
               <v-col>
-                <v-btn>텔레스테이션</v-btn>
+                <v-btn v-bind:class="{'grey': gameSelected == 'telestation'}" @click="gameSelect('telestation')">텔레스테이션</v-btn>
               </v-col>
             </v-row> 
             <div class="div3-2"> <!--div3-2 레디 --><!--세로배열-->              
               <v-col>
-                <v-btn>참여코드</v-btn>
+                <v-btn>{{ mySessionId }}</v-btn>
               </v-col>
               <v-col><v-btn>레디</v-btn></v-col>
-              <v-col><v-btn>시작</v-btn></v-col>
+              <v-col><v-btn @click="gameStart(gameSelected)">시작</v-btn></v-col>
             </div>
           </v-row>
           <div class="div4">게임설명</div><!--div4 게임설명-->
@@ -53,6 +55,7 @@ export default {
 
   data () {
 		return {
+      gameSelected: '',
 		}
 	},
 
@@ -81,6 +84,18 @@ export default {
       })
     }
 	},
+  methods : {
+    leaveSession() {
+			this.$store.dispatch('leaveSession')
+      this.$router.push('lobby')
+		},
+    gameSelect(game) {
+      this.gameSelected = game
+    },
+    gameStart(game) {
+      this.$router.push(game)
+    }
+  }
 }
 
 </script>
@@ -89,11 +104,11 @@ export default {
   position: absolute;
   bottom: 0px;
   margin: 0 auto;
+  height: 21px;
 }
 #p-name p{  
   margin: 0;
 }
-
 .playercamera {
   position: relative;
 }
@@ -103,19 +118,19 @@ export default {
   flex-direction: column;
   height: 100%;
 }*/
+.playercamera > div {
+  height: 160px;
+}
 
 video {
   max-height: 160px;
 }
-
-
 .room div {
-  height: auto;
   border: 1px solid white;  
 }
 /* 메인 화면 */
 .room .room .container {
-    height: 860px;
+  height: 860px;
 }
 /* column 패딩 0 주기 */
 .room .container .col {
@@ -123,8 +138,8 @@ video {
 }
 /* row 패딩 마진 0 주기 */
 .room .container .row {
-padding: 0;
-margin: 0;
+  padding: 0;
+  margin: 0;
 }
 
 /* 로고 있는 칸 ( 가운데 정렬)*/
@@ -132,6 +147,7 @@ margin: 0;
   box-sizing: border-box;
   text-align: center;
   border: hidden;
+  max-width: 20%;
 }
 /* 로고 */
 .room .container .div1-1 img {
@@ -139,9 +155,9 @@ margin: 0;
 }
 /* 화면 1,2,3,4 */
 .room .container .div1 .col {
-/* padding: 5px; */
-/* margin: 0; */
-height: 160px;
+  /* padding: 5px; */
+  /* margin: 0; */
+  height: 160px;
 }
 /* 화면 5,6,7,8 + 게임화면 */
 .room .container .div2 {
@@ -152,6 +168,9 @@ height: 160px;
 .room .container .div2-1 {
   border: hidden; 
   width: 20%;
+  justify-content: space-around;
+  display: flex;
+  flex-direction: column;
 }
 
 /* 화면 5,6,7,8 */
@@ -218,33 +237,40 @@ height: 160px;
 /* 1904부터는 밑의 코드 적용 */
 @media (min-width: 1904px) {
   video {
-  max-height: 180px;
+    max-height: 180px;
   }
+  .playercamera > div {
+    height: 180px;
+  }
+
   .room .container {
-  height: 935px;
+    height: 935px;
   }
   .room .container .div4 {
-  height: 570px;
+    height: 570px;
   }
   .room .container .div1 .col {
-  /* padding: 5px; */
-  height: 180px;
+    /* padding: 5px; */
+    height: 180px;
+  }
+  .room .container .div2-1 .playercamera div video {
+    max-height: 175px;
   }
   .room .container .div2-1 div {
-  /* height: 180px; */
-  /* padding: 5px; */
+    height: 180px;
+    /* padding: 5px; */
   }
 .room .container .div3-1 {
-  border: hidden; 
-  height: 150px;
+    border: hidden; 
+    height: 150px;
   }
   .room .container .div3-2  {
     border: hidden; 
     width: 200px;
   }
   .room .container .div3-2 > div {
-  width: 200px;
-  height: 50px;
+    width: 200px;
+    height: 50px;
   }
 }
 
