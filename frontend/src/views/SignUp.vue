0a123c7a6d00dfb90bd5dc9ad429376783729623
roywnsdud <roywnsdud@naver.com>
2022-01-27 17:48:34 +0900

    FEAT: 회원가입 페이지 구성

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png" alt="logo">
-    <div class="form-signup">
+    <div class="form-signup" style="form-signup">
       <label>아이디</label>
-      <input v-model="userId" class="form-control" type="text" required>
+      <input v-model="userId" type="text" name="userId" id="userId" placeholder="Id" :style="input">
+        <br>
       <label>비밀번호</label>
-      <input v-model="userPassword" class="form-control" type="text" required>
-      <label>이메일</label>
-      <input v-model="userEmail" class="form-control" type="text" required>
+        <input  v-model="userPassword" type="text" name="userPassword" id="userPassword" placeholder="Password" :style="input">
+        <br>
+        <label>이메일</label>
+      <input v-model="userEmail" type="text" name="userPasuserEmailsword" id="userEmail" placeholder="Email" :style="input">
+            <br>
       <label>닉네임</label>
-      <input v-model="userNickName" class="form-control" type="text" required>
+      <input v-model="userNickName" type="text" name="userNickName" id="userNickName" placeholder="NickName" :style="input">
+      <br>
+      <v-btn>
+      <router-link :to="{ name: 'Home' }">가입하기</router-link>
+      </v-btn>
+
     </div>  
-    <br>
-    <router-link :to="{ name: 'Home' }">가입하기</router-link> 
     <br>
   </div>
 </template>
@@ -21,7 +27,7 @@
   // import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'SignUp',    
+    name: 'Signup',    
     data () {
       return {
         userId : null,
@@ -34,3 +40,59 @@
 
 </script>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+    .form-signup {
+        background: rgba(255, 255, 255, 0.4);
+        position: absolute;
+        top: 20%;
+        left: 30%;
+        width: 40%;
+        text-align: center;
+        padding: 5px;
+        border-radius: 3rem;
+        box-shadow: 0px 0px 10px 0px;
+        padding-top: 3%;
+        padding-bottom: 5%;
+        font-family: 'Poppins', sans-serif;
+    }
+    h1 {
+        cursor: default;
+        user-select: none;
+    }
+    input {
+        border-radius: 3rem;
+        border: none;
+        padding: 10px;
+        text-align: center;
+        outline: none;
+        margin: 10px;
+        width: 30%;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    input:hover {
+        box-shadow: 0px 0px 5px 0px;
+    }
+    input:active {
+        box-shadow: 0px 0px 5px 0px;
+    }
+    #done {
+        background: lightgreen;
+    }
+    .button {
+        cursor: pointer;
+        user-select: none;
+    }
+    img {
+        width: 700px ;
+        margin: 10px;
+        user-select: none;
+    }
+    img:hover {
+        box-shadow: 0px 0px 5px 0px;
+        cursor: pointer;
+
+    }
+</style>
