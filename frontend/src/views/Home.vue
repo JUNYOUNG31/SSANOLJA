5ff7e242b71e33ec2c1c2982a09765bed6ef30ca
roywnsdud <roywnsdud@naver.com>
2022-01-29 23:34:17 +0900

    FEAT: axios 추가 및 style 변경

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,15 +3,16 @@
     <img src="../assets/logo.png" alt="logo">
       <div class="form-group">
         <div>
-        <label>아이디</label>
-        <input v-model="userId" class="form-control" type="text" required>
-        <br>
-        <br>
-        <label>비밀번호</label>
-        <input v-model="userPassword" class="form-control" type="text" required>
+          <label>아이디</label>
+          <input v-model="userId" class="form-control" type="text" required>
+          <br>
+          <label>비밀번호</label>
+          <input v-model="userPassword" class="form-control" type="text" required>
         </div>
-        <div>
-           <router-link :to="{ name: 'Lobby' }">Login</router-link> 
+        <div class="login-button">
+          <button class="btn btn-lg btn-success">
+            <router-link :to="{ name: 'Lobby' }">Login</router-link> 
+          </button>
         </div>
       </div>
     <section class="test">
@@ -22,20 +23,27 @@
     <div id="my-signin2" style="display: none"></div>
     </section>
     <br>
-    <router-link :to="{ name: 'SignUp' }">회원가입</router-link> 
+    <div class="signup-button">
+      <button class="btn btn-lg btn-primary">
+        <router-link :to="{ name: 'SignUp' }">회원가입</router-link> 
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-  // import HelloWorld from '../components/HelloWorld'
+import axios from 'axios'
+
 
   export default {
     name: 'Home',    
     data () {
       return {
+        userData : {
         userId : null,
         userPassword : null,
-      } 
+        } 
+      }
     },
     methods: {
     GoogleLoginBtn:function(){
@@ -72,6 +80,27 @@
     GoogleLoginFailure(error) {
       console.log(error);
     },
+    // axios
+    login: function () {
+      axios({
+        method: 'post',
+        url:'#',
+        data: this.userData
+      })
+        .then(res=>{
+          console.log(res)
+          // localStorage.setItem('jwt', res.data.token)
+          // this.$emit('login') 
+          this.$router.push({name:'Lobby'})
+        })
+        .catch(err=> {
+          console.log(err)
+          alert("로그인에 실패했습니다. 아이디/비밀번호를 확인하세요")
+        })
+      this.$emit('submit-login-data', this.loginData)
+    }
+
+
   }
 }    // components: {
     //   HelloWorld,
@@ -88,8 +117,8 @@
 }
 .home > img {
   width: 300px;
-  margin-top: 100px;
-  margin-bottom: 100px;
+  margin-top: 50px;
+  margin-bottom: 50px;
 }
 .googlelogin {
   display: flex;
@@ -107,6 +136,29 @@
 .form-group {
   display: flex;
 }
+
+.login-button {
+  margin-left: 30px;
+  align-self: center;
+}
+
+.login-button button {
+  width: 100px;
+  height: 100px;
+}
+.login-button button a {
+  color: white;
+}
+
+.signup-button button {
+  width: 300px;
+}
+
+.signup-button button a {
+  color: white;
+}
+
+
 
 
 </style>
