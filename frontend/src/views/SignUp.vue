6cb768eacd46e74828896d3a1a7ef0efc6a5c7ba
roywnsdud <roywnsdud@naver.com>
2022-01-29 23:38:29 +0900

    STYLE: 회원가입 페이지 css 변경

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png" alt="logo">
-    <div class="form-signup" style="form-signup">
-      <label>아이디</label>
-      <input v-model="userId" type="text" name="userId" id="userId" placeholder="Id" :style="input">
-        <br>
-      <label>비밀번호</label>
-        <input  v-model="userPassword" type="text" name="userPassword" id="userPassword" placeholder="Password" :style="input">
-        <br>
+  <div class="signup_container">
+    <div >
+      <img src="../assets/logo.png" alt="logo">
+    </div>
+    <div class="signup">
+      <h1>Sign Up</h1> 
+      <div>
+        <label>아이디</label>
+        <input v-model="userData.userId" class="form-control" type="text" id="userId" placeholder="Id" >
+      </div>
+      <div>
+        <label>비밀번호</label>
+        <input  v-model="userData.userPassword"  class="form-control" type="password"  id="userPassword" placeholder="Password" >
+      </div>
+      <div>
         <label>이메일</label>
-      <input v-model="userEmail" type="text" name="userPasuserEmailsword" id="userEmail" placeholder="Email" :style="input">
-            <br>
-      <label>닉네임</label>
-      <input v-model="userNickName" type="text" name="userNickName" id="userNickName" placeholder="NickName" :style="input">
-      <br>
-      <v-btn>
-      <router-link :to="{ name: 'Home' }">가입하기</router-link>
-      </v-btn>
+        <input v-model="userData.userEmail" class="form-control" type="text" id="userEmail" placeholder="Email" >
+      </div>
+      <div>
+        <label>닉네임</label>
+        <input v-model="userData.userNickName" class="form-control" type="text" id="userNickName" placeholder="NickName">
+      </div>
+      <div class="signup-button-to-home">
+        <button class="btn btn-lg btn-success">
+          <router-link router-link :to="{ name: 'Home' }">가입하기</router-link>
+        </button>
+      </div>
 
     </div>  
     <br>
@@ -24,75 +33,70 @@
 </template>
 
 <script>
-  // import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Signup',    
     data () {
       return {
+        userData: {
         userId : null,
         userPassword : null,
         userEmail : null,
         userNickName : null,
+        }   
       }
     },
   }
-
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-    .form-signup {
-        background: rgba(255, 255, 255, 0.4);
-        position: absolute;
-        top: 20%;
-        left: 30%;
-        width: 40%;
-        text-align: center;
-        padding: 5px;
-        border-radius: 3rem;
-        box-shadow: 0px 0px 10px 0px;
-        padding-top: 3%;
-        padding-bottom: 5%;
-        font-family: 'Poppins', sans-serif;
-    }
+.signup {
+    background: rgba(255, 255, 255, 0.4);
+    position: absolute;
+    top: 10%;
+    left: 30%;
+    width: 40%;
+    text-align: center;
+    justify-content: center;
+    padding: 5px;
+    border-radius: 3rem;
+    box-shadow: 0px 0px 10px 0px;
+    padding-top: 3%;
+    padding-bottom: 5%;
+}
     h1 {
         cursor: default;
         user-select: none;
     }
-    input {
-        border-radius: 3rem;
-        border: none;
-        padding: 10px;
-        text-align: center;
-        outline: none;
-        margin: 10px;
-        width: 30%;
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
-    }
-    input:hover {
-        box-shadow: 0px 0px 5px 0px;
-    }
-    input:active {
-        box-shadow: 0px 0px 5px 0px;
-    }
-    #done {
-        background: lightgreen;
-    }
-    .button {
-        cursor: pointer;
-        user-select: none;
-    }
-    img {
-        width: 700px ;
-        margin: 10px;
-        user-select: none;
-    }
-    img:hover {
-        box-shadow: 0px 0px 5px 0px;
-        cursor: pointer;
+    .signup_container img {
+        width: 200px ;      
+        margin-top: 50px;
 
     }
+    .signup_container img:hover {
+        box-shadow: 0px 0px 5px 0px;
+        cursor: pointer;
+    }
+
+  .signup_container .signup h1{
+  font-size: 32px;
+  margin-bottom: 28px;
+}
+.signup_container .signup input{
+  background: #333;
+border-radius: 4px;
+border: 0;
+color: white;  
+height: 50px;
+line-height: 40px;
+padding: 5px 20px;
+margin-bottom: 20px;
+width: 60%;
+text-align: center;
+margin-left: auto;
+margin-right: auto;
+}
+
+.signup-button-to-home button a {
+  color: white;
+}
 </style>
