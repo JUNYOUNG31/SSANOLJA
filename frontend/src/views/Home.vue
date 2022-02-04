23fb2aab3aa4468f58aa8a73ff5691fcf7bd9d4a
roywnsdud <roywnsdud@naver.com>
2022-02-04 13:07:44 +0900

    FEAT: 구글로그인 완료

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,105 +1,60 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png" alt="logo">
-      <div class="form-group">
-        <div>
-          <label>아이디</label>
-          <input v-model="userId" class="form-control" type="text" required>
-          <br>
-          <label>비밀번호</label>
-          <input v-model="userPassword" class="form-control" type="text" required>
-        </div>
-        <div class="login-button">
-          <router-link :to="{ name: 'Lobby' }">
-            <button class="btn btn-lg btn-success">Login</button>
-          </router-link> 
-        </div>
+    <span v-if="!isLogin">
+      <section class="test">
+      <div @click="login">      
+        <button class="btn btn-lg btn-primary grey--text">
+          <img src="../assets/googlelogo.png" alt="googlelogo" width="40">
+          Sign in with Google
+        </button>      
       </div>
-    <section class="test">
-    <div v-on:click="GoogleLoginBtn" class="googlelogin">
-      <img src="../assets/googlelogo.png" alt="googlelogo" width="40">
-      <a class="grey--text">Sign in with Google</a>      
-    </div>
-    <div id="my-signin2" style="display: none"></div>
-    </section>
-    <br>
-    <div class="signup-button">
-      <router-link :to="{ name: 'SignUp' }">
-        <button class="btn btn-lg btn-primary">회원가입</button>
-      </router-link> 
-    </div>
+      </section>
+    </span>
+    <span v-else>
+      <div>
+        <button class="btn btn-lg btn-danger"><router-link :to="{ name: 'Lobby' }">방 만들기</router-link></button>
+      </div>
+      <div>
+        <button class="btn btn-lg btn-primary" @click="loginOk">로그아웃</button>
+      </div>
+    </span>
   </div>
 </template>
-
 <script>
 import axios from 'axios'
-
 
   export default {
     name: 'Home',    
     data () {
       return {
-        userData : {
-        userId : null,
-        userPassword : null,
-        } 
+        isLogin : sessionStorage.getItem('isLogin')
       }
     },
     methods: {
-    GoogleLoginBtn:function(){
-      var self = this;
-      window.gapi.signin2.render('my-signin2', {
-        scope: 'profile email',
-        width: 240,
-        height: 50,
-        longtitle: true,
-        theme: 'dark',
-        onsuccess: this.GoogleLoginSuccess,
-        onfailure: this.GoogleLoginFailure,
-      });
-
-      setTimeout(function () {
-        if (!self.googleLoginCheck) {
-          const auth = window.gapi.auth2.getAuthInstance();
-          auth.isSignedIn.get();
-          document.querySelector('.abcRioButton').click();
-        }
-      }, 1500)
-
-    },
-    async GoogleLoginSuccess(googleUser) {
-      const googleEmail = googleUser.getBasicProfile().getEmail();
-      if (googleEmail !== 'undefined') {
-        console.log(googleEmail);
-        console.log(googleUser)
-        this.$router.push('lobby')
-      }
-    },
-    
-    //구글 로그인 콜백함수 (실패)
-    GoogleLoginFailure(error) {
-      console.log(error);
-    },
-    // axios
-    login: function () {
-      axios({
-        method: 'post',
-        url:'#',
-        data: this.userData
-      })
-        .then(res=>{
-          console.log(res)
-          localStorage.setItem('jwt', res.data.token)
-          this.$emit('login') 
-          this.$router.push({name:'Lobby'})
+      login: function () {
+        axios.get('/oauth2/authorization/google',{
+          // headers: {
+          //   'Access-Control-Allow-Origin': '*',
+          // //   'Content-Type': 'application/json; charset = utf-8'
+          // },      
+          // data: this.userData
+          // params: { redirect_url: "http://localhost:5500/lobby" }
+        })
+        .then(res => {
+          location.href = `${res.request.responseURL}`
+          // localStorage.setItem('jwt', res.data.token)
+          sessionStorage.setItem('isLogin', true);
         })
         .catch(err=> {
           console.log(err)
-          alert("로그인에 실패했습니다. 아이디/비밀번호를 확인하세요")
         })
-      this.$emit('submit-login-data', this.loginData)
+    },
+    loginOk: function() {
+      sessionStorage.clear()
+      this.$router.go('Home')
     }
-  }
+  },
 }
 </script>
 
@@ -131,12 +86,10 @@ import axios from 'axios'
 .form-group {
   display: flex;
 }
-
 .login-button {
   margin-left: 30px;
   align-self: center;
 }
-
 .login-button button {
   width: 100px;
   height: 100px;
@@ -144,16 +97,10 @@ import axios from 'axios'
 .login-button button a {
   color: white;
 }
-
 .signup-button button {
   width: 300px;
 }
-
 .signup-button button a {
   color: white;
 }
-
-
-
-
 </style>
