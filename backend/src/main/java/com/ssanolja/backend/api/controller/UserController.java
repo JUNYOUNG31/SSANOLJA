683382bd588e389b1f887b53981f3effb7af7dfa
rkdrhkddms <rhkddms3501@naver.com>
2022-02-08 14:12:04 +0900

    FIX: 로그인시 닉네임 중복 오류 해결

--- backend/src/main/java/com/ssanolja/backend/api/controller/UserController.java
+++ backend/src/main/java/com/ssanolja/backend/api/controller/UserController.java
@@ -4,6 +4,8 @@ package com.ssanolja.backend.api.controller;
 import com.ssanolja.backend.api.request.UpdateUserNicknameReq;
 import com.ssanolja.backend.api.service.UserService;
 import com.ssanolja.backend.db.entity.User;
+import com.ssanolja.backend.db.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +24,9 @@ public class UserController {
 
     @PostMapping("/update")
     public ResponseEntity updateUserNickname(@RequestBody UpdateUserNicknameReq updateUserNicknameReq) {
+        if(updateUserNicknameReq.getUpdatedUserNickname() == ""){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
         String userEmail = updateUserNicknameReq.getUserEmail();
         String updatedUserNickname = updateUserNicknameReq.getUpdatedUserNickname();
         Boolean isUpdated = userService.updateNickname(updatedUserNickname, userEmail);
