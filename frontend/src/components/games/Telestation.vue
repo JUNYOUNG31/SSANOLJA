1cab5ebe794699effa9ca5171f23afc7391ebc40
sunghyun <shcho2112@naver.com>
2022-02-10 22:14:07 +0900

    FEAT: 텔레스테이션 앨범라운드 전까지 프론트 구현

--- frontend/src/components/games/Telestation.vue
+++ frontend/src/components/games/Telestation.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="telestation-container">
     <h1>텔레스테이션</h1>
-    <!-- <button @click="getUsers">ssss</button> -->
     <div v-show="gameMode ==='text'" style="display:flex; flex-direction: column; align-items: center;"> <!-- 키워드 입력 -->
       <div>
         {{textingTime}}
@@ -9,12 +8,13 @@
         <button @click="startTexting()">시작</button>
         <button @click="pauseTexting()">정지</button>
       </div>
-      <div v-show="drawingOrder !== 1" style="width:800px; height: 400px; background-color:white; color:black;">전 사람이 그린 그림 두두등장!</div>
+      <div v-show="drawingOrder !== 1" style="width:800px; height: 400px; background-color:white; color:black;">
+        <img :src="recieveDraw" alt="받은그림">
+      </div>
       <div style="width:800px; text-align:center;">
         <label>키워드</label>
         <input v-model="keyword" class="form-control" type="text" required placeholder="키워드를 입력하세요">
         <br>
-        <!-- <input v-model="userData.userName" class="form-control" type="text" required placeholder="닉네임을 입력하세요"> -->
       </div>
       <p class="text-center">
       <button class="btn btn-lg btn-success" @click="sendMessageToEveryBody('','completed')">입력 완료</button>
@@ -23,8 +23,11 @@
     <div  v-show="gameMode ==='drawing'" style="display:flex; flex-direction: column; align-items: center;">
       {{drawingTime}}
       <div id="gamearea">
-        <canvasApi></canvasApi>
+        <canvasApi ref="canvasApi"></canvasApi>
       </div>
+      <p class="text-center">
+        <button class="btn btn-lg btn-success" @click="this.$refs.canvasAip.canavasComplete()">입력 완료</button>
+      </p> 
       </div>
       <div>{{receiveKeyword}}</div>
     
@@ -49,77 +52,33 @@ export default {
   data () {
 		return {
 			keyword: '',
+      draw:'',
       drawingTime:this.rules.drawingTime,
       textingTime:this.rules.textingTime,
+      voteTime: this.rules.voteTime,
       drawingEnabled: false,
       textingEnabled: false,
+      votingEnabled: false,
       gameMode: "drawing",
       drawingOrder:1, /* 라운드 */
       completedPlayers:0, /* 그림 다 그렸거나 or 키워드 입력완료 누른 플레이어 수 */
       readyPlayers:0, /* 전 라운드에서 키워드나 그림정보를 웹소켓으로 받은 플레이어 수*/
-      personnel:2,
+      personnel:3,
       participant: new Map(),
       targetUser: '',
-      receiveKeyword:''
+      receiveKeyword:'',
+      receiveDraw:'',
+      dataIndex:''
 		}
 	},
   methods: {
-    // postTest: function () { // back에 keyword 보내는 함수
-    // axios({
-		// 		method:'POST',
-		// 		url: '/api/telestations/InputKeyword',
-    //     data: { 
-    //       keyword: this.keyword,
-    //       userNickname: this.myUserName,
-    //       roomCode: this.mySessionId,
-    //       drawingOrder: this.drawingOrder,
-    //       personnel: this.personnel,
-    //     }
-		// 	}).then((res) => {
-    //     this.keyword = res.data.keyword,
-    //     this.userNickname = res.data.userNickname
-    //     console.log("res ----" + res);
-    //     console.log("키워드 " + this.keyword + "닉네임 " + this.userNickname)
-    //   })
-		// 	.catch(err => console.log(err))
-    // },
-    startDrawing() { //Drawing 타이머 on
-      this.drawingEnabled = true;
-    },
-    startTexting() { //Texting 타이머 on
-      this.textingEnabled = true;
-    },
-    pauseDrawing(){ //Drawing 타이머 off
-      this.drawingEnabled = false;
-    },
-    pauseTexting(){ //Texting 타이머 off
-      this.textingEnabled = false;
-    },
-    endTextRound() { // Texting 라운드 끝
-      this.pauseTexting()
-      this.textingTime = this.rules.textingTime
-      console.log("3333333333333333333333333333333333")
-      axios({
-        method:'POST',
-        url: '/api/telestations/InputKeyword',
-        data: {
-          keyword: this.keyword,
-          userNickname: this.myUserName,
-          roomCode: this.mySessionId,
-          drawingOrder: this.drawingOrder,
-          personnel: this.personnel,
-        }
-      })
-      .then((res)=> {
-        console.log("---------------------------------------------4")
-        this.targetUser = res.data.userNickname
-        this.sendMessageToTargetUser(this.keyword, "keyword", this.participant.get(this.targetUser))
-      })
-    },
-    startDrawRound() {
-      this.sendMessageToEveryBody('', "drawingOrder")
-      this.gameMode = 'drawing'
-      this.startDrawing()
+    getUsers() { // 참가자 닉네임과 connectinId 딕셔너리화 for(웹소켓)
+      let myNickName = JSON.parse(this.publisher.stream.connection.data)
+      this.participant.set(myNickName.clientData, this.publisher.stream.connection)
+      for (let index = 0; index < this.subscribers.length; index++) {
+        let nickName = JSON.parse(this.subscribers[index].stream.connection.data)
+        this.participant.set(nickName.clientData, this.subscribers[index].stream.connection)
+      }
     },
     sendMessageToEveryBody(data, type) { // 참가자 전원 웹소켓
       this.session.signal({
@@ -133,7 +92,7 @@ export default {
           console.error(error);
       })
     },
-    sendMessageToTargetUser(data, type, targetUser) { // 참가자 전원 웹소켓
+    sendMessageToTargetUser(data, type, targetUser) { // 특정 참가자 웹소켓
       this.session.signal({
           data: data,
           to: [targetUser],
@@ -144,20 +103,114 @@ export default {
           console.error(error);
       })
     },
-    getUsers() { // 참가자 닉네임과 connectinId 딕셔너리화 for(웹소켓)
-      let myNickName = JSON.parse(this.publisher.stream.connection.data)
-      this.participant.set(myNickName.clientData, this.publisher.stream.connection)
-      for (let index = 0; index < this.subscribers.length; index++) {
-        let nickName = JSON.parse(this.subscribers[index].stream.connection.data)
-        this.participant.set(nickName.clientData, this.subscribers[index].stream.connection)
-      }
-      console.log(this.publisher)
-      console.log(this.participant, '과여양아ㅏ아아아앙')
-      console.log(this.participant.get('조성현'))
+    // 타이머 함수 모음 시작
+    startDrawing() { //Drawing 타이머 on
+      this.drawingEnabled = true;
+    },
+    startTexting() { //Texting 타이머 on
+      this.textingEnabled = true;
+    },
+    pauseDrawing(){ //Drawing 타이머 off
+      this.drawingEnabled = false;
+    },
+    pauseTexting(){ //Texting 타이머 off
+      this.textingEnabled = false;
+    },
+    startAlbum(){
+      this.votingEnabled = true;
+    },
+    pauseAlbum(){
+      this.votingEnabled = false;
+    },
+
+    // startTimer(){
+    //   if (gmaeMode === 'drawing') {
+    //     this.drawingEnabled = true;
+    //   }
+    //   else if (gmaeMode === 'keyword') {
+    //     this.textingEnabled = true;
+    //   }
+    //   else if (gmaeMode === 'vote') {
+    //     this.votingEnabled = true;
+    //   }
+    // },
+    // 타이머 함수 모음 끝
+
+    // 캔버스 관련 함수 시작
+    canvasComplete(){
+      this.sendMessageToEveryBody('','completed')
+    },
+    setDrawData(imageData){
+      this.draw = imageData
+    },
+    // 캔버스 관련 함수 시작
+
+    //라운드 끝내기& 시작 함수 모음 시작
+    endTextRound() { // Texting 라운드 끝
+      this.pauseTexting()
+      this.textingTime = this.rules.textingTime
+      axios({
+        method:'POST',
+        url: '/api/telestations/InputKeyword',
+        data: {
+          keyword: this.keyword,
+          userNickname: this.myUserName,
+          roomCode: this.mySessionId,
+          drawingOrder: this.drawingOrder,
+          personnel: this.personnel,
+        }
+      })
+      .then((res)=> {
+        this.targetUser = res.data.userNickname
+        this.sendMessageToTargetUser(JSON.stringify(res.data), "keyword", this.participant.get(this.targetUser))
+      })
+    },
+    endDrawRound() { // Drawing 라운드 끝
+      this.pauseDrawing()
+      this.DrawingTime = this.rules.DrawingTime
+      this.$refs.canvasApi.saveClick()
+      // canvas에서 그림 축출해서 this.darw에 저장
+      axios({
+        method:'POST',
+        url: '/api/telestations/InputKeyword',
+        data: {
+          draw: this.draw,
+          userNickname: this.myUserName,
+          roomCode: this.mySessionId,
+          drawingOrder: this.drawingOrder,
+          personnel: this.personnel,
+          dataIndex: this.dataIndex,
+        }
+      })
+      .then((res)=> {
+        this.targetUser = res.data.userNickname
+        this.sendMessageToTargetUser(JSON.stringify(res.data), "draw", this.participant.get(this.targetUser))
+      })
     },
     startTextRound() {
-      console.log('안들어와야해')
-    }
+      this.sendMessageToEveryBody('', "drawingOrder")
+      if (this.drawingOrder === this.personnel +1) {
+        this.startAlbum()
+      }else {
+      this.gameMode = 'text'
+      this.startTexting()
+      }
+    },
+    startDrawRound() {
+      this.sendMessageToEveryBody('', "drawingOrder")
+      if (this.drawingOrder === this.personnel +1) {
+        this.startAlbum()
+      }else {
+        this.gameMode = 'drawing'
+        this.startDrawing()
+      }
+    },
+    startAlbum(){
+      console.log('앨범라운드')
+    },
+    //라운드 끝내기& 시작 함수 모음 끝
+
+
 
 // this.sendMessageToEveryBody(JSON.stringify(this.gameRes), 'gameRes')
   },
@@ -171,7 +224,7 @@ export default {
     ]),
   },
   watch: {
-    // Drawing
+    // Drawing timer
     drawingEnabled(value) {
           if (value) {
               setTimeout(() => {
@@ -188,10 +241,13 @@ export default {
                     this.drawingTime--;
                 }, 1000);
             }
-
+            if (value === 0) {
+              this.endDrawRound()
+            }
         },
-        immediate: false // This ensures the watcher is triggered upon creation
+        immediate: false
     },
+    //Text timer
     textingEnabled(value) {
           if (value) {
               setTimeout(() => {
@@ -210,24 +266,29 @@ export default {
               this.endTextRound()
             }
         },
-        immediate: false // This ensures the watcher is triggered upon creation
+        immediate: false
     },
 
-    completedPlayers: {
+    completedPlayers: { // 제출완료
       handler(value) {
         if (value === this.personnel && this.gameMode==='text') {
           this.completedPlayers = 0
           this.endTextRound()
         }
+        if (value === this.personnel && this.gameMode==='drawing') {
+          this.completedPlayers = 0
+          this.endDrawRound()
+        }
       }
     },
-    readyPlayers: {
+    readyPlayers: { // 웹소켓 수신 완료 => 다음라운드로 가기위한 변수
       handler(value) {
         if (value === this.personnel && this.gameMode==='text') {
           this.readyPlayers = 0
           this.startDrawRound()
         }
         if (value === this.personnel && this.gameMode==='drawing') {
+          this.readyPlayers = 0
           this.startTextRound()
         }
       }
@@ -236,20 +297,27 @@ export default {
   mounted() {
     this.startTexting()
     this.getUsers()
-    this.session.on('signal:keyword', (event) => {
-      console.log(event.data)
-      this.receiveKeyword = event.data
+    this.session.on('signal:keyword', (event) => { // 입력한 키워드 백에 보내기
+      let data = JSON.parse(event.data)
+      this.receiveKeyword = data.keyword
+      this.dataIndex = data.dataIndex
       this.sendMessageToEveryBody('', "ready")
     })
-    this.session.on('signal:completed', (event) => {
+    this.session.on('signal:draw', (event) => { // 그린 그림 백에 보내기 
+      let data = JSON.parse(event.data)
+      this.receiveDraw = data.draw
+      this.dataIndex = data.dataIndex
+      this.sendMessageToEveryBody('', "ready")
+    })
+    this.session.on('signal:completed', (event) => { // 입력버튼 누르면 완료된 사람 +1
       console.log(event.data)
       this.completedPlayers++
     })
-    this.session.on('signal:ready', (event) => {
+    this.session.on('signal:ready', (event) => { // 백에서 데이터 받아서 웹소켓 통신하고 나면 +1
       console.log(event.data)
       this.readyPlayers++
     })
-    this.session.on('signal:drawingOrder', (event) => {
+    this.session.on('signal:drawingOrder', (event) => { // 각 라운드 시작시 라운드 +1
       console.log(event.data)
       this.drawingOrder++
     })
