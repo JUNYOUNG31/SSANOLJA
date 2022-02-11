05c44008f72d580c921f3a1b7b39a31fc9644c00
roywnsdud <roywnsdud@naver.com>
2022-02-11 11:10:10 +0900

    FIX: 한명이 투표시 모든사람이 투표불가 현상 수정

--- frontend/src/components/games/Spyfall.vue
+++ frontend/src/components/games/Spyfall.vue
@@ -148,12 +148,12 @@ export default {
       timerCount: 30,
       votetimeCnt: 30,
       voteEnabled: false,
+      isVoted : false,
       voteList : {
-        voteCnt : 0,
-        isVoted : false,
+        voteCnt : 0, 
         agreeCnt: 0,
         disagreeCnt: 0,
-      },  
+      },      
       isEnded: false,
       isSpy: false,
       spyName: null,
@@ -240,14 +240,14 @@ export default {
       con.style.display = (con.style.display!= 'none') ? "none":"block"
     },
     voteTrue() {
-      this.voteList.isVoted = true
+      this.isVoted = true
       this.sendMessageToEveryBody(JSON.stringify(this.voteList), 'voteTrue')     
       console.log('여기')    
       console.log(this.myUserName)
       console.log(JSON.parse(this.votePlayer.stream.connection.data).clientData)
     },
     voteFalse() {
-      this.voteList.isVoted = true
+      this.isVoted = true
       this.sendMessageToEveryBody(JSON.stringify(this.voteList), 'voteFalse')       
     }
   },
@@ -367,7 +367,7 @@ export default {
       this.voteList.voteCnt = 0
       this.voteList.agreeCnt = 0
       this.voteList.disagreeCnt = 0
-      this.voteList.isVoted = false
+      this.isVoted = false
     })  
   }
 }
