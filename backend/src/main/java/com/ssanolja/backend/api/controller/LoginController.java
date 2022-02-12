afcbac40ceda2547d43f490077ed836a4e685a0c
sunghyun <shcho2112@naver.com>
2022-02-13 04:19:20 +0900

    FIX: 병합 후 에러해결

--- backend/src/main/java/com/ssanolja/backend/api/controller/LoginController.java
+++ backend/src/main/java/com/ssanolja/backend/api/controller/LoginController.java
@@ -1,16 +1,9 @@
 package com.ssanolja.backend.api.controller;
 
-import com.ssanolja.backend.api.service.LoginService;
-import com.ssanolja.backend.api.service.UserService;
 import com.ssanolja.backend.db.entity.User;
 import com.ssanolja.backend.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/login")
@@ -37,8 +30,8 @@ public class LoginController {
     }
 
     @PostMapping("/sendUser")
-    public ResponseEntity senduser(@RequestBody String userEmail){
-        User res = loginService.senduser(userEmail);
-        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+    public User senduser(@RequestBody String userEmail){
+        User user = userRepository.findByUserEmail(userEmail);
+        return user;
     }
 }
