fef17f2492f5f93904cad0e54711d0b0b024cb4b
roywnsdud <roywnsdud@naver.com>
2022-02-13 20:39:37 +0900

    STYLE: 홈페이지 style 변경

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png" alt="logo">
+    <div class="bouncing-text">
+      <div class="S1"><h1>S</h1></div>
+      <div class="S2"><h1>S</h1></div>
+      <div class="A1"><h1>A</h1></div>
+      <div class="N"><h1>N</h1></div>
+      <div class="O"><h1>O</h1></div>
+      <div class="L"><h1>L</h1></div>
+      <div class="J"><h1>J</h1></div>
+      <div class="A2"><h1>A</h1></div>
+      <div class="shadow"></div>
+      <div class="shadow-two"></div>
+      <div class="shadow-three"></div>
+      <div class="shadow-four"></div>
+    </div>
     <section class="test">
     <div v-on:click="GoogleLoginBtn">
-      <button class="paper-btn btn-lg btn-warning black--text d-flex" >
-          <img src="../assets/googlelogo.png" alt="googlelogo" width="40">
+      <button class="paper-btn btn-lg black--text d-flex" style="width: 500px; height: 250px" >
+          <img src="../assets/googlelogo.png" alt="googlelogo" width="130px" style="margin-top: 45px">
           <p 
-          style="margin: 8px 0 8px; font-family: Patrick Hand SC, sans-serif; font-weight: normal;" witdh="40">
+          style="margin: 80px 0 8px; font-family: Patrick Hand SC, sans-serif; font-weight: normal; font-size: 50px" witdh="370px">
           Sign in with Google
           </p> 
         </button> 
@@ -19,20 +32,19 @@
 <script>
   // import HelloWorld from '../components/HelloWorld'
 import axios from 'axios'
-  export default {
-    name: 'Home',
-    data() {
-      return {
-        userData : {
-      userEmail : null,
-      userNickname : null,
-            },
-      }
-    },
-    methods: {
+export default {
+  name: 'Home',
+  data() {
+    return {
+      userData : {
+        userEmail : null,
+        userNickname : null,
+      },
+    }
+  },
+  methods: {
     GoogleLoginBtn:function(){
       var self = this;
-
       window.gapi.signin2.render('my-signin2', {
         scope: 'profile email',
         width: 240,
@@ -42,7 +54,6 @@ import axios from 'axios'
         onsuccess: this.GoogleLoginSuccess,
         onfailure: this.GoogleLoginFailure,
       });
-
       setTimeout(function () {
         if (!self.googleLoginCheck) {
           const auth = window.gapi.auth2.getAuthInstance();
@@ -50,7 +61,6 @@ import axios from 'axios'
           document.querySelector('.abcRioButton').click();
         }
       }, 1500)
-
     },
     async GoogleLoginSuccess(googleUser) {
       const googleEmail = googleUser.getBasicProfile().getEmail();
@@ -66,40 +76,33 @@ import axios from 'axios'
       }
       this.insertUser()
     },
-    //구글 로그인 콜백함수 (실패)
+      //구글 로그인 콜백함수 (실패)
     GoogleLoginFailure(error) {
       console.log(error);
     },
     insertUser: function() {
       axios({
-                method:'POST',
-                url: '/api/login/postTest',
-         data: { 
-           userEmail : this.userData.userEmail,
-           userNickname : this.userData.userNickname
-         }
-            }).then(res => {
-        console.log(res.data)
-                if(res.data == "ok"){
-           this.$router.push( { name: 'Lobby', params: { sendUserEmail: this.userData.userEmail}})
-         }
-            })
-      // .then(res => {
-      //   console.log(res)
-      //   this.$router.push( { name: 'Lobby', params: { sendUserEmail: this.userData.userEmail}})
-      // })
-            .catch(err => console.log(err))
-
+        method:'POST',
+        url: '/api/login/postTest',
+        data: { 
+          userEmail : this.userData.userEmail,
+          userNickname : this.userData.userNickname
+        }
+      })
+      .then(res => {  
+        if (res.data == "ok") {
+          this.$router.push( { name: 'Lobby', params: { sendUserEmail: this.userData.userEmail}})
+        }
+      })
+      .catch(err => console.log(err))
     },
   }
-}    // components: {
-    //   HelloWorld,
-    // },
-
+}
 </script>
 
 <style>
 .home {
+  margin-top: 50px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -109,5 +112,147 @@ import axios from 'axios'
   width: 300px;
   margin-top: 100px;
   margin-bottom: 100px;
+}
+/* 로고 */
+.bouncing-text {
+  position: relative;
+  display: flex;  
+  font-family: "Patrick Hand SC", sans-serif;
+  margin-bottom: 120px;
+  margin-top: 120px;
+}
+
+.S1, .S2, .A1, .N, .O, .A2 {
+  color: rgb(0, 0, 0);
+  margin-right: 20px;
+  text-shadow: 0 3px #a1a1a1, 0 5px #a1a1a1, 0 7px #a1a1a1;
+}
+
+.L {
+  color: rgb(0, 0, 0);
+  margin-right: 25px;
+  text-shadow: 0 3px #a1a1a1, 0 5px #a1a1a1, 0 7px #a1a1a1;
+}
+
+.J {
+  color: rgb(0, 0, 0);
+  margin-right: 25px;
+  text-shadow: 0 3px #a1a1a1, 0 5px #a1a1a1, 0 7px #a1a1a1;
+}
+
+.S1 h1, .S2 h1, .A1 h1, .N h1, .O h1, .L h1, .J h1, .A2 h1 {
+  margin: 0;
+  font-size: 250px;
+}
+
+@keyframes bounce {
+        0%   { transform: scale(1,1) translateY(0); }
+        10%  { transform: scale(1.1,.9) translateY(0); }
+        30%  { transform: scale(.9,1.1)   translateY(-55px);}
+        50%  { transform: scale(1.05,.95) translateY(0); }
+        58%  { transform: scale(1,1) translateY(-7px); }
+        65%  { transform: scale(1,1) translateY(0);}
+        100% { transform: scale(1,1) translateY(0);}
+    }
+
+.S1 {
+  animation: bounce 2s ease infinite;
+}
+
+.S2 {
+  animation: bounce 2s ease infinite .2s;
+}
+
+.A1 {
+  animation: bounce 2s ease infinite .4s;
+}
+
+.N {
+  animation: bounce 2s ease infinite .6s;
+}
+
+.O {
+  animation: bounce 2s ease infinite .8s;
+}
+
+.L {
+  animation: bounce 2s ease infinite 1.0s;
+}
+
+.J {
+  animation: bounce 2s ease infinite 1.2s;
+}
+
+.A2 {
+  animation: bounce 2s ease infinite 1.4s;
+}
+
+
+.shadow, .shadow-two, .shadow-three, .shadow-four {
+  position: absolute;
+  bottom: 0;
+}
+
+.shadow:before, .shadow:after, .shadow-two:before, .shadow-two:after, .shadow-three:before, .shadow-three:after, .shadow-four:before, .shadow-four:after {
+  content:"";
+  position: absolute;
+  background-color: rgba(0,0,0,0.4);
+  width: 114px;
+  height:5px;
+  border-radius:50%;
+  top: -20px;  
+}
+
+.bouncing-text:before {
+  bottom: 0;
+  animation: scale 2s linear infinite;
+}
+
+@keyframes scale {
+  0% {transform: scaleX(1);}
+  25% {transform: scaleX(0.4);}
+  50% {transform: scaleX(1);}
+  75% {transform: scaleX(0.9);}
+  100% {transform: scaleX(1);}
+}
+
+.shadow:before {
+  left: 0;
+  animation: scale 2s linear infinite .4s;
+}
+
+.shadow:after {
+  left:135px;
+  animation: scale 2s linear infinite .6s;
+}
+
+.shadow-two:before {
+  left:276px;
+  animation: scale 2s linear infinite .8s;
+}
+
+.shadow-two:after {
+  left: 420px;
+  animation: scale 2s linear infinite 1.0s;
+}
+
+.shadow-three:before {
+  left: 580px;
+  animation: scale 2s linear infinite 1.2s;
+}
+
+.shadow-three:after {
+  left: 710px;
+  animation: scale 2s linear infinite 1.4s;
+}
+
+.shadow-four:before {
+  left:830px;
+  animation: scale 2s linear infinite 1.2s;
+}
+
+.shadow-four:after {
+  left: 960px;
+  animation: scale 2s linear infinite 1.4s;
 }
 </style>
